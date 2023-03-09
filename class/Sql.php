df123b46dd9f128941299604a94debbce450a230
jacson_lauro@hotmail.com <jacson_lauro@hotmail.com>
2023-03-09 18:57:27 -0300

    criamos a casse Usuario

--- class/Sql.php
+++ class/Sql.php
@@ -1,55 +1,34 @@
 <?php
 
-class Sql extends PDO {
+class Sql {
 
     private $conn;
 
     public function __construct() {
-
         $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
-
     }
 
     private function setParams($statement, $parameters = array()) {
-
         foreach ($parameters as $key => $value) {
-
-            $this->setParam($statement, $key, $value);
-
+            $this->bindParam($statement, $key, $value);
         }
-
     }
 
-    private function setParam($statement, $key, $value){
-
+    private function bindParam($statement, $key, $value) {
         $statement->bindParam($key, $value);
-
     }
 
-    public function query(string $query, ?int $fetchMode = null, ...$fetchModeArgs): PDOStatement|false {
+    public function query(string $rawQuery, array $params = []): PDOStatement|false {
+    $stmt = $this->conn->prepare($rawQuery);
+    $this->setParams($stmt, $params);
+    $stmt->execute();
+    return $stmt;
+}
 
-        $stmt = $this->conn->prepare($query);
 
-        if (!$stmt) {
-            return false;
-        }
-
-        if ($fetchMode !== null) {
-            $stmt->setFetchMode($fetchMode, ...$fetchModeArgs);
-        }
-
-        return $stmt->execute() ? $stmt : false;
-
-    }
-
-    public function select($rawQuery, $params = array()):array{
-
-        $stmt = $this->query($rawQuery);
-
-        $this->setParams($stmt, $params);
-
+    public function select($rawQuery, $params = array()):array {
+        $stmt = $this->query($rawQuery, $params);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
     }
 
 }
