893c696e6c745ad4b00f92f222b6590b8148f9a9
jacson_lauro@hotmail.com <jacson_lauro@hotmail.com>
2023-03-11 18:52:20 -0300

    Inserir e Alterar

--- class/Sql.php
+++ class/Sql.php
@@ -5,30 +5,46 @@ class Sql {
     private $conn;
 
     public function __construct() {
+
         $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+
     }
 
     private function setParams($statement, $parameters = array()) {
+
         foreach ($parameters as $key => $value) {
-            $this->bindParam($statement, $key, $value);
+
+            $this->setParam($statement, $key, $value);
+
         }
+
     }
 
-    private function bindParam($statement, $key, $value) {
+    private function setParam($statement, $key, $value){
+
         $statement->bindParam($key, $value);
+
     }
 
-    public function query(string $rawQuery, array $params = []): PDOStatement|false {
-    $stmt = $this->conn->prepare($rawQuery);
-    $this->setParams($stmt, $params);
-    $stmt->execute();
-    return $stmt;
-}
+    public function query($rawQuery, $params = array()) {
 
+        $stmt = $this->conn->prepare($rawQuery);
 
-    public function select($rawQuery, $params = array()):array {
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+
+    }
+
+    public function select($rawQuery, $params = array()):array
+    {
+
         $stmt = $this->query($rawQuery, $params);
+
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     }
 
 }
--- index.php
+++ index.php
@@ -2,23 +2,37 @@
 
 require_once("config.php");
 
-//Carrega um usúario
+//Carrega um usuário
 //$root = new Usuario();
 //$root->loadbyId(3);
 //echo $root;
 
-//Carregta uma lista de usúarios
+//Carrega uma lista de usuários
 //$lista = Usuario::getList();
-
 //echo json_encode($lista);
-//carrega uma lista de usuários buscado pelo login
-//$search = Usuario::search("jo");
 
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
 //echo json_encode($search);
-//carrega um usuário usando o login e a senha
+
+//Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+
+//echo $usuario;
+
+/*
+Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
 
 $usuario = new Usuario();
-$usuario->login("root", "Jr1Is@c2");
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
 
 echo $usuario;
 
